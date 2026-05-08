b3bb698931e76bd7849c848c14ae8e65d7dd46db
Harshclouder <kumar2796shiv@gmail.com>
2026-05-08 22:34:09 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello , i am harsh");
+        System.out.println("Hello");
+         System.out.println("Harsh");
+         System.out.println("Prajapati");
     }
 }
